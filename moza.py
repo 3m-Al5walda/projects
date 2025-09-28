fdb80b92f0e4d991f5f92e0b9c62d508324090fc
3bdulla al5walda <mabdull606@gmail.com>
2025-09-28 19:27:18 +0300

    MOZA
    this is moza full screen

---
+++ moza.py
@@ -0,0 +1,30 @@
+import os 
+import time
+def mozaaaa() :
+    t = 20
+    while not False :
+        print('\n' * t)
+        print('\t          ---====D                        @               ')
+        print('\t o                 ,,*:*::,.__     *                        ')
+        print('\t              * ,)))*))))* __.=-.             o            ')
+        print('\t      |       ,((*((o o*))-\'                                ')
+        print('\t     -O-    ,))))))\ = /                     =( =     +      ')
+        print('\t+     |    ,(((*(((()-(               *                       ')
+        print('\t           *))))))/    `\                                      ')
+        print('\t           (((*}(//(_|_)\                        .             ')
+        print('\t           )*))))\*).( //            +  .                       ')
+        print('\t  *        (((((*(// / )/                   |                    ')
+        print('\t            ))))))/ / /`                   -O-     @             ')
+        print('\t+           `(*(((/ /`                      |                    ')
+        print('\t          o  `)))*))`\            *                              ')
+        print('\t               `((((  `-.__     ,      .                         ')
+        print('\t     .            *)-._    `--~`                              ')
+        print('\t @                +    `~---~~`                *                 ')
+        print('\t                                                                ')
+        time.sleep(0.1)
+        os.system('cls')
+        t -= 1
+        if t < 0 :
+            t =20
+    
+mozaaaa()
